d0a2eabb1cc317bfe7d7fb3e2f4e9cff4770517c
abhiperfect <abhishekprajapati.890e@gmail.com>
2024-03-21 20:37:55 +0530

    vercel link

--- SERVER/database/connectToDatabase.js
+++ SERVER/database/connectToDatabase.js
@@ -1,24 +1,44 @@
 import pg from "pg";
 import { password } from "../constants/constants.js";
 // Function to connect to the PostgreSQL database
+// function connectToDatabase() {
+//     const db = new pg.Client({
+//         user: "postgres",
+//         host: "localhost",
+//         database: "tuf",
+//         password: password,
+//         port: 5432,
+//     });
+
+//     db.connect()
+//         .then(() => {
+//             console.log('Connected to the PostgreSQL database');
+//         })
+//         .catch((error) => {
+//             console.error('Error connecting to the PostgreSQL database:', error);
+//         });
+
+//     return db;
+// }
+
+const { Pool } = pg;
+
 function connectToDatabase() {
-    const db = new pg.Client({
-        user: "postgres",
-        host: "localhost",
-        database: "tuf",
-        password: password,
-        port: 5432,
+  const pool = new Pool({
+    connectionString:
+      "postgres://default:0tuwH9cDFRdQ@ep-still-sunset-a4icuau8-pooler.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require",
+  });
+
+  pool
+    .connect()
+    .then(() => {
+      console.log("Connected to the PostgreSQL database");
+    })
+    .catch((error) => {
+      console.error("Error connecting to the PostgreSQL database:", error);
     });
 
-    db.connect()
-        .then(() => {
-            console.log('Connected to the PostgreSQL database');
-        })
-        .catch((error) => {
-            console.error('Error connecting to the PostgreSQL database:', error);
-        });
-
-    return db;
+  return pool;
 }
 
 export default connectToDatabase;
