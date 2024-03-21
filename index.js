b93d8d7a918cd16a59e084a2a0d3dbe5597790e9
abhiperfect <abhishekprajapati.890e@gmail.com>
2024-03-21 22:36:37 +0530

    css path updated 4

--- index.js
+++ index.js
@@ -29,10 +29,10 @@ const port = 3000;
 
 
 //FOR IMPORTING CLIENT DATA INTO SERVER
-const __filename = fileURLToPath(import.meta.url); //GET THE CURRENT FILE PATH
-const __dirname = dirname(__filename); //GET THE DIRECTORY PATH OF THE CURRENT FILE
+// const __filename = fileURLToPath(import.meta.url); //GET THE CURRENT FILE PATH
+// const __dirname = dirname(__filename); //GET THE DIRECTORY PATH OF THE CURRENT FILE
 app.set("views", path.join(__dirname, "CLIENT/views")); //SET VIEWS PATH
-app.use(express.static(__dirname + "CLIENT/public"));
+app.use(express.static(path.join(__dirname, "CLIENT/public")));
 app.set("view engine", "ejs");
 
 
