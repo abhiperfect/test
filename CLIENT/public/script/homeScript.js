b03f59f3ab4889e328d8fd3b1254784d22392644
abhiperfect <abhishekprajapati.890e@gmail.com>
2024-03-21 19:47:15 +0530

    second commit

--- CLIENT/public/script/homeScript.js
+++ CLIENT/public/script/homeScript.js
@@ -1,16 +1,1 @@
 
-    function validateForm() {
-      var username = document.getElementById("username").value.trim();
-      var codeLanguage = document.getElementById("codeLanguage").value.trim();
-      var sourceCode = document.getElementById("sourceCode").value.trim();
-      var errorMessage = document.getElementById("errorMessage");
-
-      if (username === "" || codeLanguage === "" || sourceCode === "") {
-        errorMessage.innerHTML = "All fields are required.";
-        return false; // Prevent form submission
-      }
-
-      return true; // Allow form submission
-    }
-
-    
